c5ddf9de8acf8b412b8ed8e7952233542e4ef660
Peter Souter <p.morsou@gmail.com>
2016-04-13 11:22:26 +0100

    Add rpmforge acceptance test

---
+++ spec/acceptance/rpmforge_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper_acceptance'
+
+describe 'rhel_mrepo_profiles::repo_mirror::rpmforge class' do
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'install mrepo' do
+      pp = <<-EOS
+      class { 'rhel_mrepo_profiles': }
+      EOS
+
+      apply_manifest(pp, :catch_failures  => true)
+      apply_manifest(pp, :catch_failures  => true)
+    end
+
+    it 'should work idempotently with no errors' do
+      pp = <<-EOS
+      class { 'rhel_mrepo_profiles::repo_mirror::rpmforge': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures  => true)
+      apply_manifest(pp, :catch_changes   => true)
+    end
+
+  end
+end
