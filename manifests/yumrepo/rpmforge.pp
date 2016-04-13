9bfcb05540bad1b684d9d8cbdc39df2cbadaef6b
Peter Souter <p.morsou@gmail.com>
2016-04-13 10:56:11 +0100

    Adds yumrepo helper class for rpmforge

---
+++ manifests/yumrepo/rpmforge.pp
@@ -0,0 +1,18 @@
+# Add Yumrepo for Oracle Linux Mirror
+class rhel_mrepo_profiles::yumrepo::oracle
+(
+  $mrepo_master,
+)
+{
+
+  yumrepo { 'rpmforge':
+    baseurl  => "http://${mrepo_master}:${mrepo_port}/rpmforge-${::operatingsystemmajrelease}-${::architecture}/RPMS.all",
+    descr    => 'RHEL $releasever - RPMforge.net - dag',
+    enabled  => 1,
+    gpgcheck => 0,
+    proxy    => 'absent',
+  }
+
+}
+
+
